aeea429250bc7baede86d48631e0babaa9abc570
Dimebagou <bersonvictor@gmail.com>
2023-05-04 16:36:12 +0200

    Adding Routes folder so app.js stay readable

---
+++ Routes/worksManagers.js
@@ -0,0 +1,41 @@
+import express from 'express';
+import { getWorksManagers, getWorksManagerById, createWorksManager, updateWorksManager, deleteWorksManagerById } from '../Querries/worksManagers.js';
+
+const router = express.Router();
+
+// Retourne tous les worksManagers sous la forme d'un tableau d'objets {id, firstname, lastname, mail}
+router.get('/', async (req, res) => {
+    const worksManagers = await getWorksManagers();
+    res.send(worksManagers);
+});
+
+// Retourne un seul worksManager sous la forme d'un objet {id, firstname, lastname, mail}
+router.get('/:id', async (req, res) => {
+    const id = req.params.id;
+    const worksManager = await getWorksManagerById(id);
+    res.send(worksManager);
+});
+
+// Ajoute un worksManager
+router.post('/', async (req, res) => {
+    const { firstname, lastname, mail } = req.body;
+    const worksManager = await createWorksManager(firstname, lastname, mail);
+    res.status(201).send(worksManager);
+});
+
+// Modifie un worksManager
+router.put('/:id', async (req, res) => {
+    const id = req.params.id;
+    const { firstname, lastname, mail } = req.body;
+    const worksManager = await updateWorksManager(id, firstname, lastname, mail);
+    res.send(worksManager);
+});
+
+// Supprime un worksManager en fonction de son id
+router.delete('/:id', async (req, res) => {
+    const id = req.params.id;
+    const worksManager = await deleteWorksManagerById(id);
+    res.send(worksManager);
+});
+
+export default router;
--- app.js
+++ app.js
@@ -1,54 +1,19 @@
 import express from 'express';
+import worksManagersRoutes from './Routes/worksManagers.js';
 
-
-// imports des différentes querries de la base de données en fonction de leur table
-import { getWorksManagers, getWorksManagerById, createWorksManager, updateWorksManager, deleteWorksManagerById } from './worksManagers/querries.js';
 
 const app = express();
-
 app.use(express.json());
 
-// récupère les workmanagers au format tableau d'objets (id, firstname, lastname, mail, login, password)
-app.get('/api/works_managers', async (req, res) => {
-    const worksManagers = await getWorksManagers();
-    res.send(worksManagers);
-}) 
+// Routes //
+app.use('/api/works_managers', worksManagersRoutes);
 
-// récupère le workmanager au format objet (id, firstname, lastname, mail, login, password)
-app.get('/api/works_managers/:id', async (req, res) => {
-    const id = req.params.id;
-    const worksManager = await getWorksManagerById(id);
-    res.send(worksManager);
-})
-
-// crée un nouveau workmanager
-app.post('/api/works_managers', async (req, res) => {
-    const { firstname, lastname, mail } = req.body;
-    const worksManager = await createWorksManager(firstname, lastname, mail);
-    res.status(201).send(worksManager);
-})
-
-// modifie un nouveau workmanager
-app.put('/api/works_managers/:id', async (req, res) => {
-    const id = req.params.id;
-    const { firstname, lastname, mail} = req.body;
-    const worksManager = await updateWorksManager(id, firstname, lastname, mail);
-    res.send(worksManager);
-})
-
-// supprime un workmanager en fonction de l'id
-app.delete('/api/works_managers/:id', async (req, res) => {
-    const id = req.params.id;
-    const worksManager = await deleteWorksManagerById(id);
-    res.send(worksManager);
-})
-
-// gestion de l'erreur : https://expressjs.com/en/guide/error-handling.html
+// gestion des erreurs copiée/collée depuis ce lien : https://expressjs.com/en/guide/error-handling.html
 app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).send('Something broke!');
-})
+});
 
 app.listen(8080, () => {
     console.log('Server running on port 8080');
-})
+});
